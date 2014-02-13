ab6896df7cb987b09b3799a869a4562992d2a7a0
Gabriel Martinez <reitaka@gmail.com>
2014-02-12 16:06:31 -0800

    Add method to get collision Tiles from Map
    Also add Rectangle class, used in this new method.
    
    Finished Episode 13.1.

--- src/map.py
+++ src/map.py
@@ -1,9 +1,25 @@
 import game
 from sprite import Sprite
 
+class TileType:
+    AIR_TILE = 0
+    WALL_TILE = 1
+    COUNT = 2
+
+class Tile:
+    def __init__(self, tileType=TileType.AIR_TILE, sprite=None):
+        self.tileType = tileType
+        self.sprite = sprite
+
+class CollisionTile:
+    def __init__(self, row, col, tileType):
+        self.row = row
+        self.col = col
+        self.tileType = tileType
+
 class Map:
     def __init__(self):
-        self.foregroundSprites = []
+        self.tiles = []
 
     @staticmethod
     def createTestMap(graphics):
@@ -11,33 +27,48 @@ class Map:
 
         numRows = 15 # 15 * 32 = 480
         numCols = 20 # 20 * 32 = 640
-        # Ensure foregroundSprites is numRows x numCols in size
-        ret.foregroundSprites = [[None]*numCols for _ in range(numRows)]
+        # Ensure tiles is numRows x numCols in size
+        ret.tiles = [[Tile() for _ in range(numCols)] for _ in range(numRows)]
 
         sprite = Sprite(graphics,
                         b'content/PrtCave.bmp',
                         game.TILE_SIZE, 0,
                         game.TILE_SIZE, game.TILE_SIZE)
+        tile = Tile(TileType.WALL_TILE, sprite)
         row = 11
         for col in range(numCols):
-            ret.foregroundSprites[row][col] = sprite
-        ret.foregroundSprites[10][5] = sprite
-        ret.foregroundSprites[9][4] = sprite
-        ret.foregroundSprites[8][3] = sprite
-        ret.foregroundSprites[7][2] = sprite
-        ret.foregroundSprites[10][3] = sprite
+            ret.tiles[row][col] = tile
+        ret.tiles[10][5] = tile
+        ret.tiles[9][4] = tile
+        ret.tiles[8][3] = tile
+        ret.tiles[7][2] = tile
+        ret.tiles[10][3] = tile
 
         return ret
 
+    def getCollidingTiles(self, rectangle):
+        firstRow = rectangle.top() // game.TILE_SIZE
+        lastRow = rectangle.bottom() // game.TILE_SIZE
+        firstCol = rectangle.left() // game.TILE_SIZE
+        lastCol = rectangle.right() // game.TILE_SIZE
+
+        collisionTiles = []
+        for row in range(firstRow, lastRow + 1):
+            for col in range(firstCol, lastCol + 1):
+                tileType = self.tiles[row][col].tileType
+                collisionTiles.append(CollisionTile(row, col, tileType))
+
+        return collisionTiles
+
     def update(self, elapsedTime):
-        for row in range(len(self.foregroundSprites)):
-            for col in range(len(self.foregroundSprites[row])):
-                if self.foregroundSprites[row][col]:
-                    self.foregroundSprites[row][col].update(elapsedTime)
+        for row in range(len(self.tiles)):
+            for col in range(len(self.tiles[row])):
+                if self.tiles[row][col].sprite:
+                    self.tiles[row][col].sprite.update(elapsedTime)
 
     def draw(self, graphics):
-        for row in range(len(self.foregroundSprites)):
-            for col in range(len(self.foregroundSprites[row])):
-                if self.foregroundSprites[row][col]:
-                    self.foregroundSprites[row][col].draw(
+        for row in range(len(self.tiles)):
+            for col in range(len(self.tiles[row])):
+                if self.tiles[row][col].sprite:
+                    self.tiles[row][col].sprite.draw(
                             graphics, col*game.TILE_SIZE, row*game.TILE_SIZE)
---
+++ src/rectangle.py
@@ -0,0 +1,19 @@
+# TODO: consider making a namedtuple?
+class Rectangle:
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+    def left(self):
+        return self.x
+
+    def right(self):
+        return self.x + self.width
+
+    def top(self):
+        return self.y
+
+    def bottom(self):
+        return self.y + self.height
