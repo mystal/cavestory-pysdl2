13675758664fcb05578782a3f4eeb8fe9af35064
Gabriel Martinez <reitaka@gmail.com>
2014-02-20 08:53:19 -0800

    Refine and clean up Player movement
    Use constants gotten from the original Cave Story. Allows us to
    remove the Jump class and to have smoother movement.
    
    Finished Episode 14.

--- src/player.py
+++ src/player.py
@@ -7,17 +7,18 @@ from rectangle import Rectangle
 from sprite import Sprite
 
 # Walk Motion
-SLOWDOWN_FACTOR = 0.8
-WALKING_ACCELERATION = 0.0012 # (pixels / ms) / ms
-MAX_SPEED_X = 0.325 # pixels / ms
+WALKING_ACCELERATION = 0.00083007812 # (pixels / ms) / ms
+MAX_SPEED_X = 0.15859375 # pixels / ms
+FRICTION = 0.00049804687 # (pixels / ms) / ms
 
 # Fall Motion
-GRAVITY = 0.0012 # (pixels / ms) / ms
-MAX_SPEED_Y = 0.325 # pixels / ms
+GRAVITY = 0.00078125 # (pixels / ms) / ms
+MAX_SPEED_Y = 0.2998046875 # pixels / ms
 
 # Jump Motion
-JUMP_SPEED = 0.325 # pixels / ms
-JUMP_TIME = 275 # ms
+JUMP_SPEED = 0.25 # pixels / ms
+AIR_ACCELERATION = 0.0003125 # (pixels / ms) / ms
+JUMP_GRAVITY = 0.0003125 # (pixels / ms) / ms
 
 # Sprites
 SPRITE_FILE_PATH = b'content/MyChar.bmp'
@@ -43,10 +44,11 @@ COLLISION_Y = Rectangle(10, 2, 12, 30)
 
 class MotionType:
     STANDING = 0
-    WALKING = 1
-    JUMPING = 2
-    FALLING = 3
-    COUNT = 4
+    INTERACTING = 1
+    WALKING = 2
+    JUMPING = 3
+    FALLING = 4
+    COUNT = 5
 
 class HorizontalFacing:
     LEFT = 0
@@ -83,40 +85,20 @@ class SpriteState(namedtuple('SpriteState',
         return super().__new__(
                 cls, motionType, horizontalFacing, verticalFacing)
 
-class Jump:
-    def __init__(self):
-        self.timeRemaining = 0
-        self.active = False
-
-    def update(self, elapsedTime):
-        if self.active:
-            self.timeRemaining -= elapsedTime
-            if self.timeRemaining <= 0:
-                self.active = False
-
-    def reset(self):
-        self.timeRemaining = JUMP_TIME
-        self.reactivate()
-
-    def reactivate(self):
-        self.active = self.timeRemaining > 0
-
-    def deactivate(self):
-        self.active = False
-
 class Player:
     def __init__(self, graphics, x, y):
         self.x = x
         self.y = y
         self.velocityX = 0.0
         self.velocityY = 0.0
-        self.accelerationX = 0.0
+        self.accelerationX = 0
 
         self.horizontalFacing = HorizontalFacing.LEFT
         self.verticalFacing = VerticalFacing.HORIZONTAL
         self._onGround = False
 
-        self.jump = Jump()
+        self.jumpActive = False
+        self.interacting = False
 
         self.sprites = {}
         self.initializeSprites(graphics)
@@ -140,6 +122,8 @@ class Player:
             sourceX = WALK_FRAME * game.TILE_SIZE
         elif spriteState.motionType == MotionType.STANDING:
             sourceX = STAND_FRAME * game.TILE_SIZE
+        elif spriteState.motionType == MotionType.INTERACTING:
+            sourceX = BACK_FRAME * game.TILE_SIZE
         elif spriteState.motionType == MotionType.JUMPING:
             sourceX = JUMP_FRAME * game.TILE_SIZE
         elif spriteState.motionType == MotionType.FALLING:
@@ -156,18 +140,17 @@ class Player:
                 graphics, SPRITE_FILE_PATH, sourceX, sourceY,
                 game.TILE_SIZE, game.TILE_SIZE, WALK_FPS, NUM_WALK_FRAMES)
         else:
-            if spriteState.verticalFacing == VerticalFacing.DOWN:
-                if spriteState.motionType == MotionType.STANDING:
-                    sourceX = BACK_FRAME * game.TILE_SIZE
-                else:
-                    sourceX = DOWN_FRAME * game.TILE_SIZE
+            if (spriteState.verticalFacing == VerticalFacing.DOWN and
+                spriteState.motionType in (MotionType.JUMPING, MotionType.FALLING)):
+                sourceX = DOWN_FRAME * game.TILE_SIZE
             self.sprites[spriteState] = Sprite(
                 graphics, SPRITE_FILE_PATH, sourceX, sourceY,
                 game.TILE_SIZE, game.TILE_SIZE)
 
     def getSpriteState(self):
-        motion = None
-        if self.onGround():
+        if self.interacting:
+            motion = MotionType.INTERACTING
+        elif self.onGround():
             motion = MotionType.STANDING if self.accelerationX == 0 else MotionType.WALKING
         else:
             motion = MotionType.JUMPING if self.velocityY < 0 else MotionType.FALLING
@@ -203,20 +186,25 @@ class Player:
 
     def update(self, elapsedTime, map):
         self.sprites[self.getSpriteState()].update(elapsedTime)
-        self.jump.update(elapsedTime)
 
         self.updateX(elapsedTime, map)
         self.updateY(elapsedTime, map)
 
     def updateX(self, elapsedTime, map):
         # Update Velocity
-        self.velocityX += self.accelerationX * elapsedTime
+        accelerationX = (WALKING_ACCELERATION if self.onGround()
+                                              else AIR_ACCELERATION)
+        self.velocityX += self.accelerationX * accelerationX * elapsedTime
+
         if self.accelerationX < 0:
             self.velocityX = max(self.velocityX, -MAX_SPEED_X)
         elif self.accelerationX > 0:
             self.velocityX = min(self.velocityX, MAX_SPEED_X)
         elif self._onGround:
-            self.velocityX *= SLOWDOWN_FACTOR
+            if self.velocityX > 0:
+                self.velocityX = max(0.0, self.velocityX - FRICTION * elapsedTime)
+            else:
+                self.velocityX = min(0.0, self.velocityX + FRICTION * elapsedTime)
 
         # Calculate Delta
         delta = round(self.velocityX * elapsedTime)
@@ -255,9 +243,10 @@ class Player:
 
     def updateY(self, elapsedTime, map):
         # Update Velocity
-        if not self.jump.active:
-            self.velocityY += GRAVITY * elapsedTime
-            self.velocityY = min(self.velocityY, MAX_SPEED_Y)
+        gravity = (JUMP_GRAVITY if self.jumpActive and self.velocityY < 0
+                                else GRAVITY)
+        self.velocityY += gravity * elapsedTime
+        self.velocityY = min(self.velocityY, MAX_SPEED_Y)
 
         # Calculate Delta
         delta = round(self.velocityY * elapsedTime)
@@ -302,34 +291,39 @@ class Player:
         self.sprites[self.getSpriteState()].draw(graphics, self.x, self.y)
 
     def startMovingLeft(self):
-        self.accelerationX = -WALKING_ACCELERATION
+        self.accelerationX = -1
         self.horizontalFacing = HorizontalFacing.LEFT
+        self.interacting = False
 
     def startMovingRight(self):
-        self.accelerationX = WALKING_ACCELERATION
+        self.accelerationX = 1
         self.horizontalFacing = HorizontalFacing.RIGHT
+        self.interacting = False
 
     def stopMoving(self):
         self.accelerationX = 0
 
     def lookUp(self):
         self.verticalFacing = VerticalFacing.UP
+        self.interacting = False
 
     def lookDown(self):
+        if self.verticalFacing == VerticalFacing.DOWN:
+            return
         self.verticalFacing = VerticalFacing.DOWN
+        self.interacting = self.onGround()
 
     def lookHorizontal(self):
         self.verticalFacing = VerticalFacing.HORIZONTAL
 
     def startJump(self):
+        self.jumpActive = True
         if self.onGround():
-            self.jump.reset()
             self.velocityY = -JUMP_SPEED
-        elif self.velocityY > 0:
-            self.jump.reactivate()
+        self.interacting = False
 
     def stopJump(self):
-        self.jump.deactivate()
+        self.jumpActive = False
 
     def onGround(self):
         return self._onGround
