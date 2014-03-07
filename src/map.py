8c5ee946bd371e348f3a9f569d6a27d0c75e4cf4
Gabriel Martinez <reitaka@gmail.com>
2014-03-07 08:50:53 -0800

    Add support for background tiles to Map
    Finished Episode 16.

--- src/map.py
+++ src/map.py
@@ -20,8 +20,9 @@ class CollisionTile:
 
 class Map:
     def __init__(self):
-        self.tiles = []
         self.backdrop = None
+        self.tiles = []
+        self.backgroundTiles = []
 
     @staticmethod
     def createTestMap(graphics):
@@ -31,8 +32,9 @@ class Map:
 
         numRows = 15 # 15 * 32 = 480
         numCols = 20 # 20 * 32 = 640
-        # Ensure tiles is numRows x numCols in size
+        # Ensure tiles and backgroundTiles are numRows x numCols in size
         ret.tiles = [[Tile() for _ in range(numCols)] for _ in range(numRows)]
+        ret.backgroundTiles = [[None for _ in range(numCols)] for _ in range(numRows)]
 
         sprite = Sprite(graphics,
                         b'content/PrtCave.bmp',
@@ -47,6 +49,23 @@ class Map:
         ret.tiles[8][3] = tile
         ret.tiles[7][2] = tile
         ret.tiles[10][3] = tile
+
+        chainTop = Sprite(graphics,
+                          b'content/PrtCave.bmp',
+                          11*game.TILE_SIZE, 2*game.TILE_SIZE,
+                          game.TILE_SIZE, game.TILE_SIZE)
+        chainMiddle = Sprite(graphics,
+                             b'content/PrtCave.bmp',
+                             12*game.TILE_SIZE, 2*game.TILE_SIZE,
+                             game.TILE_SIZE, game.TILE_SIZE)
+        chainBottom = Sprite(graphics,
+                             b'content/PrtCave.bmp',
+                             13*game.TILE_SIZE, 2*game.TILE_SIZE,
+                             game.TILE_SIZE, game.TILE_SIZE)
+
+        ret.backgroundTiles[8][2] = chainTop
+        ret.backgroundTiles[9][2] = chainMiddle
+        ret.backgroundTiles[10][2] = chainBottom
 
         return ret
 
@@ -72,6 +91,11 @@ class Map:
 
     def drawBackground(self, graphics):
         self.backdrop.draw(graphics)
+        for row in range(len(self.backgroundTiles)):
+            for col in range(len(self.backgroundTiles[row])):
+                if self.backgroundTiles[row][col]:
+                    self.backgroundTiles[row][col].draw(
+                            graphics, col*game.TILE_SIZE, row*game.TILE_SIZE)
 
     def draw(self, graphics):
         for row in range(len(self.tiles)):
